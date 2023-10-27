e8486d3726d5ffd2c66989f4d9f656166051eaef
Md Jaowad Hasan <mdjaowadhasan@Mds-MacBook-Pro.local>
2023-10-27 10:48:29 -0700

    new Bar Chart Added

--- bar_chart.py
+++ bar_chart.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
 
 # Sample data
 categories = ['A', 'B', 'C', 'D', 'E']
@@ -13,4 +15,15 @@ plt.ylabel('Values')
 plt.title('Bar Chart')
 
 # Show the plot
+plt.show()
+
+objects = ('Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp')
+y_pos = np.arange(len(objects))
+performance = [10,8,6,4,2,1]
+
+plt.bar(y_pos, performance, align='center', alpha=0.5)
+plt.xticks(y_pos, objects)
+plt.ylabel('Usage')
+plt.title('Programming language usage')
+
 plt.show()
