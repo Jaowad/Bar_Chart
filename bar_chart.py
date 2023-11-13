8e21574df2345f75e03515ecdb1bb4cf1e6156c6
Md Jaowad Hasan <mdjaowadhasan@Mds-MacBook-Pro.local>
2023-11-12 21:47:35 -0800

    Added horizontal Bar Chart

--- bar_chart.py
+++ bar_chart.py
@@ -17,6 +17,7 @@ plt.title('Bar Chart')
 # Show the plot
 plt.show()
 
+#vertical bar chart
 objects = ('Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp')
 y_pos = np.arange(len(objects))
 performance = [10,8,6,4,2,1]
@@ -25,5 +26,17 @@ plt.bar(y_pos, performance, align='center', alpha=0.5)
 plt.xticks(y_pos, objects)
 plt.ylabel('Usage')
 plt.title('Programming language usage')
+
+plt.show()
+
+#Horizontal Bar chart
+objects = ('A', 'B', 'C')
+y_pos = np.arange(len(objects))
+performance = [10,9,5]
+
+plt.barh(y_pos, performance, align='center', alpha=0.5)
+plt.yticks(y_pos,objects)
+plt.xlabel('Value')
+plt.title('Horizontal bar Chart')
 
 plt.show()
